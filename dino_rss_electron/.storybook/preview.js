4d99aa889456b73b30652ffb233b68e07d6d8e30
DRG3930 <davidrglass3930@outlook.com>
2025-01-20 02:57:54 -0800

    delete files

--- dino_rss_electron/.storybook/preview.js
+++
@@ -1,6 +0,0 @@
-import '../public/css/materialize.min.css';
-import '../public/css/global.css';
-import '../public/css/prism.css';
-import './debug.css';
-
-import '../public/podplayer/embed.js'
--- gh_ost/localtests/rename-none-column/create.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists gh_ost_test;
-create table gh_ost_test (
-  id int auto_increment,
-  c1 int not null,
-  primary key (id)
-) auto_increment=1;
-
-drop event if exists gh_ost_test;
