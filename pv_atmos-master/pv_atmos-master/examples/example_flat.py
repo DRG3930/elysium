fe41d5a7b1b30d9e01793bcde680d70d834da073
DRG3930 <davidrglass3930@outlook.com>
2025-01-20 02:58:50 -0800

    adding atmos

---
+++ pv_atmos-master/pv_atmos-master/examples/example_flat.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+# Filename: example_flat.py
+#
+# Code by Martin Jucker, distributed under an MIT License
+# Any publication benefitting from this piece of code should cite
+# Jucker, M 2014. Scientific Visualisation of Atmospheric Data with ParaView.
+# Journal of Open Research Software 2(1):e4, DOI: http://dx.doi.org/10.5334/jors.al
+
+######################################################################################
+# set path to the locations of pv_atmos/grids.py, pv_atmos/basic.py, and the examples folder
+# you can define this in the session or here
+try:
+    pvAtmosPath
+except:
+    pvAtmosPath = raw_input("Please provide the path where pv_atmos lives: ")
+    pvAtmosPath = pvAtmosPath+'/'
+try: #is pv_atmos installed?
+    from pv_atmos.basic import *
+    from pv_atmos.grids import *
+except:
+    execfile(pvAtmosPath + 'basic.py')
+    execfile(pvAtmosPath + 'grids.py')
+# define aspect ratio of coordinate system: keep lon and lat, multiply log-p by 20. Base log-p on 1e3hPa
+ratio = [1,1,20]
+logCoord = [2]
+basis = [1e3]
+
+## now load example file ##
+fileName = pvAtmosPath + 'examples/uv_daily.nc'
+# check if files exist
+import os.path
+if not os.path.isfile(fileName):
+    raise ValueError, fileName+' does not exist!'
+# the file is 3D+time, in pressure coordinates, and we adjust the axis aspect ratio
+(output_nc,Coor) = LoadData(fileName, ['lon','lat','pfull'], ratio, logCoord, basis)
+# we have now read in a 4D file, with log-pressure in the Z-direction
+
+# as an example, apply contour filter on zonal wind to show tropospheric jet streams and stratospheric polar vortex
+# then, add contour at 25m/s
+Cont = Contour(ContourBy='ucomp',Isosurfaces=[25])
+# make sure ucomp is still available for coloring. Paraview version dependent.
+try:
+    Cont.ComputeScalars = 1
+except:
+    pass
+# color it
+repU = Show()
+# ColorBy, Paraview >v5
+try:
+    ColorBy(repU,('POINTS','ucomp'))
+    lkpU = GetColorTransferFunction('ucomp')
+    lkpU.RescaleTransferFunction(-50.,50.)
+except:
+    repU.ColorArrayName = 'ucomp'
+    # get lookup table for coloring ucomp, this only works in ParaView >= v4.1
+    try:
+        ucomp = output_nc.PointData.GetArray('ucomp')
+        lkpU = AssignLookupTable(ucomp,'Cool to Warm')
+        repU.LookupTable = lkpU
+    except:
+        pass
+# make it transparent
+repU.Opacity = 0.7
+Show()
+
+# now we want to add arrows showing the horizontal wind vector
+(W,normW,clipWS,clipWN) = CartWind2Sphere(src=Coor,ratios=ratio)
+# add arrows
+Arrows = Glyph()
+Arrows.Scalars = ['POINTS','normW']
+Arrows.Vectors = ['POINTS','W']
+Arrows.ScaleMode = 'scalar'
+# often the glyphs are too large by default. make them smaller
+Arrows.ScaleFactor = 0.5
+# make sure seed is the same for reproducibility
+Arrows.Seed = 10339
+repA = Show()
+repA.ColorArrayName = 'GlyphVector'
+# also, create a colormap lookup table for future use. Again, only >= v4.1
+try:
+    wind = Arrows.PointData.GetArray('GlyphVector')
+    lkpW = AssignLookupTable(wind,'X Ray')
+    lkpW.ColorSpace = 'Lab'
+    repA.LookupTable = lkpW
+except:
+    pass
+
+## add axes and grid
+LabSze = 6.0
+Bounds = [0,360,-90,90,1e3,0.01]
+# outside box, rename axes
+AddGrid(xlevels=[0,360],ylevels=[-90,90],zlevels=[1e3,0.1],bounds=Bounds, ratios=ratio, logCoords=logCoord, basis=basis, AxisWidth=2.0,LabelSize=LabSze, AxisNames=["longitude","latitude","pressure [hPa]"])
+# inside grid lines: add only line labels, not axis labels
+AddGrid(xlevels=[90,180,270],ylevels=[-45,0,45],zlevels=[1e2,10,1],bounds=Bounds,ratios=ratio, logCoords=logCoord, basis=basis, AxisWidth=1.0,LabelSize=-LabSze)
+
+# add a plane showing wind stength at 100hPa
+Plane100=AddGridPlane(2, 100,Bounds,ratio,logCoord,basis,data=1,src=normW)
+RenameSource("Plane100",Plane100)
+Plane100rep = GetDisplayProperties(Plane100)
+Plane100rep.ColorArrayName = 'normW'
+try:
+    Plane100rep.LookupTable = lkpW
+except:
+    pass
+Plane100rep.Opacity = 0.7
+# add a plane showing zonal wind strength at 180 longitude
+Plane180=AddGridPlane(0, 180,Bounds,ratio,logCoord,basis,data=1,src=normW)
+RenameSource("Plane180E",Plane180)
+Plane180rep = GetDisplayProperties(Plane180)
+Plane180rep.ColorArrayName = 'ucomp'
+try:
+    Plane180rep.LookupTable = lkpU
+except:
+    pass
+Plane180rep.Opacity = 0.7
+# add a plane showing meridional wind strength at the equator
+# PlaneEQ=AddGridPlane(1, 0,Bounds,ratio,logCoord,basis,data=1,src=normW)
+# RenameSource("PlaneEQ",PlaneEQ)
+# PlaneEQrep = GetDisplayProperties(PlaneEQ)
+# PlaneEQrep.ColorArrayName = 'vcomp'
+# try:
+#     vcomp = output_nc.PointData.GetArray('vcomp')
+#     lkpV = AssignLookupTable(vcomp,'bone_Matlab')
+#     PlaneEQrep.LookupTable = lkpV
+# except:
+#     pass
+# PlaneEQrep.Opacity = 0.7
+
+# some camera settings
+(Xmin,Xmax,Ymin,Ymax,Zmin,Zmax) = BoundAspectRatio(Bounds,ratio,logCoord,basis)
+view=GetActiveView()
+view.CameraPosition = [2*Xmin,0.5*(Ymin+Ymax),Zmax*1.2]
+view.CameraFocalPoint = [0.5*(Xmin+Xmax),0.5*(Ymin+Ymax),0.5*(Zmin+Zmax)]
+view.CameraParallelProjection = 0
+view.CameraViewAngle = 30
+view.CameraViewUp = [0,0,1]
+
+Render()
---
+++ pv_atmos-master/pv_atmos-master/examples/example_ocean.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python
+# Filename: example_ocean.py
+#
+# Code by Martin Jucker, distributed under an MIT License
+# Any publication benefitting from this piece of code should cite
+# Jucker, M 2014. Scientific Visualisation of Atmospheric Data with ParaView.
+# Journal of Open Research Software 2(1):e4, DOI: http://dx.doi.org/10.5334/jors.al
+
+######################################################################################
+# set path to the locations of grids.py, basic.py, and the examples folder
+# you can define this in the session or here
+try:
+    pvAtmosPath
+except:
+    pvAtmosPath = raw_input("Please provide the path where pv_atmos lives: ")
+    pvAtmosPath = pvAtmosPath+'/'
+try: #is pv_atmos installed?
+    from pv_atmos.basic import *
+    from pv_atmos.grids import *
+except:
+    execfile(pvAtmosPath + 'basic.py')
+    execfile(pvAtmosPath + 'grids.py')
+
+## show me where the files are, relative to pvAtmosPath ##
+# path to ocean files
+oceanPath = pvAtmosPath + 'examples/'
+
+# file containing bathymetry
+topoFile = 'ocean_depth.nc'
+topoDims = ['rlon','rlat']
+depthVar = 'deptho'
+
+# file containing oxygen data
+dataFile = 'ocean_o2.nc'
+dataDims = ['xt_ocean','yt_ocean','st_ocean']
+# the values we will be interested in
+dataName = 'o2'
+dataContours = [8e-5]
+
+## how would you like the transformation to work ##
+logCoord = [] #no logarithmic coordinates
+aspRat   = [1,1,0.01] # divide bathymetry by 100
+
+
+### get the data ###
+
+# check if files exist
+import os.path
+if not os.path.isfile(oceanPath+topoFile):
+    raise ValueError, oceanPath+topoFile+' does not exist!'
+if not os.path.isfile(oceanPath+dataFile):
+    raise ValueError, oceanPath+dataFile+' does not exist!'
+
+
+# bathymetry
+(depth_out,depth_coor)=LoadData(oceanPath+topoFile,ncDims=topoDims,logCoords=logCoord,replaceNaN=False )
+# get the bounds of the topography. This is important if bathymetry and data files have different origins
+topoBds = depth_out.GetDataInformation().GetBounds()
+
+# data
+(o2_out,o2_coor)=LoadData(oceanPath+dataFile,ncDims=dataDims,aspectRatios=aspRat,logCoords=logCoord )
+# we want to replace the fill values with NaNs here
+o2_out.ReplaceFillValueWithNan = 1
+# get the bounds of the data file. Again important if different from bathymetry files
+dataBds = o2_out.GetDataInformation().GetBounds()
+# instead of NaNs, there are -1e10 values in this file that we don't want
+o2_thresh = Threshold(o2_coor,ThresholdRange=[0,1])
+MakeSelectable()
+
+# the bathymetry file is in cell data, need to convert to point data
+c2p=CellDatatoPointData(depth_coor)
+MakeSelectable()
+
+# now make the bathymetry 3D
+bathy = Make3D(depthVar, expandDir='-z', aspectRatios=aspRat, logCoords=logCoord, src=c2p)
+rep=GetDisplayProperties(bathy)
+# paraview >=v5
+try:
+    ColorBy(rep,('POINTS',depthVar))
+    depthoLUT = GetColorTransferFunction(depthVar)
+    depthoLUT.ApplyPreset('erdc_blue_BW',True)
+    depthoLUT.InvertTransferFunction()
+except:
+    rep.ColorArrayName = depthVar
+    # this works only for paraview > 4.1
+    try:
+        depthVal = bathy.PointData.GetArray(depthVar)
+        lkpD = AssignLookupTable(depthVal,'erdc_blue_BW')
+        #invert the colors
+        valPts=lkpD.RGBPoints[::4]
+        lkpD.RGBPoints[::4]=valPts[::-1]
+        rep.LookupTable = lkpD
+    except:
+        pass
+
+
+#### now add data to the ocean #######
+o2_cont = Contour(o2_thresh, ContourBy=['POINTS',dataName], Isosurfaces=dataContours)
+rep=Show()
+# paraview >=v5
+try:
+    o2_cont.ComputeScalars = 1
+    ColorBy(rep,('POINTS',dataName))
+    o2LUT = GetColorTransferFunction(dataName)
+    o2LUT.ApplyPreset('GnYlRd',True)
+    o2LUT.RescaleTransferFunction(0.,1.e-4)
+except:
+    rep.ColorArrayName = dataName
+    try:
+        dataVal = o2_cont.PointData.GetArray(dataName)
+        lkpW = AssignLookupTable(dataVal,'GnYlRd')
+        rep.LookupTable = lkpW
+    except:
+        pass
+
+#### finally, add a grid  ####
+AddGrid(xlevels=[-270,-225,-180,-135,-90,-45,0,45], ylevels=[-60,-30,0,30,60], zlevels=range(-5000,1000,1000), bounds=[-280,80,-90,90,-5500,0], ratios=aspRat, logCoords=logCoord, AxisNames=["longitude","latitude","depth [m]"], AxisColor=[0,0,0], AxisWidth=1.0,LabelSize=5.0)
+
+
+Render()
